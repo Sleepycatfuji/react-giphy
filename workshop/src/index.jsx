086de1b173b15b04fda86702a7bf5280d4709d3d
Duangrudee Methakullachat <blufairie888@gmail.com>
2022-04-11 09:56:48 +0900

    I said Hello World in React

--- workshop/src/index.jsx
+++ workshop/src/index.jsx
@@ -1,1 +1,21 @@
-import '../assets/stylesheets/application.scss';
+import "../assets/stylesheets/application.scss";
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+// const Hello = (props) => {
+//   const { name, age } = props;
+//   const message = `Hello, I'm ${name} and I'm ${age}!`;
+//   return <h1>{message}</h1>;
+// };
+
+class Hello extends React.Component {
+  render() {
+    const { name, age } = this.props;
+    const message = `Hello, I'm ${name} and I'm ${age}!`;
+    return <h1>{message}</h1>;
+  }
+}
+
+const root = document.getElementById("root");
+ReactDOM.render(<Hello name="Kim the Great" age="33" />, root);
